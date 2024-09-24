34eef71a187468d44c45aa527f6b9e4a715e0618
MuhammadHanifYousafzai <m.hanifjhanzaibian@gmail.com>
2024-09-24 14:48:45 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-3xl text-red-900'>HostelEase</p>
+      <p className='text-3xl text-yellow-800'> we provide you with the best Hotels in the town</p>
+    </div>
+      
+  );
+}
+
+export default App;
