ef2cdb81d9e81813db287ed37f881486299fa821
MuhammadHanifYousafzai <m.hanifjhanzaibian@gmail.com>
2024-09-25 16:31:47 +0500

    about  us

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Layout from "./layout";
 
 function App() {
   return (
-    <div className="App">
-      <p className='text-3xl text-red-900'>HostelEase</p>
-      <p className='text-3xl text-yellow-800'> we provide you with the best Hotels in the town</p>
-    </div>
-      
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route
+            path="/home"
+            element={
+              <Layout>
+                <Home />
+              </Layout>
+            }
+          />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
